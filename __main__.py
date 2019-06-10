24172e1ddacfebf835f8569347a7e7cbde2cecce
Michal <pikusa.michal@gmail.com>
2019-06-10 14:37:21 +0200

    fixed importing preprocess with correct path

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 """Main module."""
 
 import os
-from keywords_extractor.data.preprocessing import preprocess
+from keywords_extractor.preprocessing.preprocessing import preprocess
 from keywords_extractor.model.textrank import process_with_textrank
 from keywords_extractor.model.rake import process_with_rake
 
