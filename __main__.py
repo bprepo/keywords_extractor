6f69a8e2912d50a0b6de833156ca315ac200e360
Bartosz Baranowski <bartosz.baranowski.5@gmail.com>
2019-06-10 23:14:15 +0200

    first approach to flair

--- __main__.py
+++ __main__.py
@@ -3,9 +3,10 @@
 """Main module."""
 
 import os
-from keywords_extractor.data.preprocessing import preprocess
+from keywords_extractor.preprocessing.preprocessing import preprocess
 from keywords_extractor.model.textrank import process_with_textrank
 from keywords_extractor.model.rake import process_with_rake
+from keywords_extractor.model.flair import process_with_flair
 
 
 def main(args=None):
@@ -13,10 +14,10 @@ def main(args=None):
     project_path = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(project_path, "data", "test_data.txt")
     doc = preprocess(file_path, "en")
-    for sent in doc.sents:
-        print(sent)
-    print(process_with_textrank(doc.text))
-    print(process_with_rake(doc.text))
+    flair = process_with_flair(doc.text)
+    for token in flair:
+        print(token)
+        print(token.embedding)
 
 
 if __name__ == "__main__":
